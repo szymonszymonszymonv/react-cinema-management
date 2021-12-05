0b05ed7e7dc95ade15c3bdb95d21a3c7bb4e671d
szymonszymonszymonv <kalarepka8799@gmail.com>
2021-12-06 00:40:31 +0100

    fix checking time diff

--- src/WyswietlSeans.js
+++ src/WyswietlSeans.js
@@ -23,7 +23,7 @@ function WyswietlSeans(props) {
 
     for (let i = 0; i < screenings.length; i++) {
         let timeInt = screenings[i].time.split(":").map((x) => { return parseInt(x) })
-        if (zmienna.getDate() === screenings[i].date.getDate() && timeInt[0] >= timeInts[0]) {
+        if (zmienna.getDate() === screenings[i].date.getDate() && timeInt[0] >= timeInts[0] && timeInt[1] >= timeInts[1]) {
             wypisac.push(screenings[i])
         }
     }
