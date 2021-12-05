e6fb06a966881eb06634ca17d5edd6c58a177add
szymonszymonszymonv <kalarepka8799@gmail.com>
2021-12-06 00:52:55 +0100

    fix time comparison

--- src/WyswietlSeans.js
+++ src/WyswietlSeans.js
@@ -23,7 +23,10 @@ function WyswietlSeans(props) {
 
     for (let i = 0; i < screenings.length; i++) {
         let timeInt = screenings[i].time.split(":").map((x) => { return parseInt(x) })
-        if (zmienna.getDate() === screenings[i].date.getDate() && timeInt[0] >= timeInts[0] && timeInt[1] >= timeInts[1]) {
+        if (zmienna.getDate() === screenings[i].date.getDate() && timeInt[0] >= timeInts[0]) {
+            if(timeInt[0] === timeInts[0] && timeInt[1] < timeInts[1]){
+                continue
+            }
             wypisac.push(screenings[i])
         }
     }
