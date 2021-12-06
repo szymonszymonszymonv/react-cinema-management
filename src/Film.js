f58b394db29c2a220d4484c19166d22ec1c1cd08
szymonszymonszymonv <kalarepka8799@gmail.com>
2021-12-06 01:49:39 +0100

    fix value

--- src/Film.js
+++ src/Film.js
@@ -112,23 +112,23 @@ function Film(props) {
                 {"Obsada: "} {listaFilmow[idx].cast}{" "}
               </p>{" "}
             </div>
-        <input type="text" placeholder={listaFilmow[idx].title} onChange={onChange} />{" "}
+        <input type="text" value={listaFilmow[idx].title} onChange={onChange} />{" "}
         <button onClick={buttonClick}> Edytuj tytuł </button> <p> </p>{" "}
         <input
           type="text"
-          placeholder={listaFilmow[idx].description}
+          value={listaFilmow[idx].description}
           onChange={onChangenDescription}
         />{" "}
         <button onClick={buttonClickDescription}> Edytuj opis </button> <p> </p>{" "}
         <input
           type="text"
-          placeholder={listaFilmow[idx].duration}
+          value={listaFilmow[idx].duration}
           onChange={onChangeDuration}
         />{" "}
         <button onClick={buttonClickDuration}> Edytuj czas </button> <p> </p>{" "}
         <input
           type="text"
-          placeholder={listaFilmow[idx].cast}
+          value={listaFilmow[idx].cast}
           onChange={onChangenCast}
         />{" "}
         <button onClick={buttonClickCast}> Edytuj obsadę </button> <p> </p>{" "}
